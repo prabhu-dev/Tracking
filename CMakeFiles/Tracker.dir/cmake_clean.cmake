2a5428e430b6efc2caf8ba030fd0f545608f1159
Prabhudev Prakash <pprakash30@gatech.edu>
2016-05-11 03:12:36 -0400

    Initial Commit

---
+++ CMakeFiles/Tracker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Tracker.dir/tracker.cpp.o"
+  "Tracker.pdb"
+  "Tracker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
